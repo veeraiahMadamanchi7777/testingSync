309716f1e568bf60332e5c980df2a4099bf37fb6
veeraiah Madamanchi <veeraiahmadamanchi@gmail.com>
2024-06-06 22:11:31 +0000

    Update file main.tf

--- main.tf
+++ main.tf
@@ -10,14 +10,22 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region = "us-west-2"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "ExampleVPC"
+  }
+}
+
+resource "aws_subnet" "example_subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "ExampleSubnet"
   }
 }
